a0d6f65fce9ffa59955bed12166011ca7cff25d9
hairongchen <hairong.chen@intel.com>
2023-07-18 12:14:12 +0800

    quote-server/Dockerfile: update to use api and proto dir

--- service/quote-server/build.rs
+++ service/quote-server/build.rs
@@ -2,7 +2,7 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/quote_server.proto")?;
+    tonic_build::compile_protos("api/quote-server.proto")?;
 
     let original_out_dir = PathBuf::from(env::var("OUT_DIR")?);
     let out_dir = "./src";
@@ -10,7 +10,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .out_dir(out_dir)
         .file_descriptor_set_path(original_out_dir.join("quote_server_descriptor.bin"))
-        .compile(&["proto/quote_server.proto"], &["proto"])?;
+        .compile(&["api/quote-server.proto"], &["api"])?;
 
     Ok(())
 }
