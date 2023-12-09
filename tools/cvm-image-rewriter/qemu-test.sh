e393941b2220f4a1d57bd34ef2a9a0b3eecff980
Lu Ken <ken.lu@intel.com>
2023-12-09 16:10:10 +0800

    cvm-image-rewriter: clean up qemu-test.sh to use common.sh
    Signed-off-by: Lu Ken <ken.lu@intel.com>

--- tools/cvm-image-rewriter/qemu-test.sh
+++ tools/cvm-image-rewriter/qemu-test.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
+TOP_DIR="$(dirname $(readlink -f "$0"))"
+SCRIPTS_DIR="${TOP_DIR}/scripts"
+
 INPUT_IMG="output.qcow2"
 
-info() {
-    echo -e "\e[1;33m$*\e[0;0m"
-}
-
-ok() {
-    echo -e "\e[1;32mSUCCESS: $*\e[0;0m"
-}
-
-error() {
-    echo -e "\e[1;31mERROR: $*\e[0;0m"
-    exit 1
-}
-
-warn() {
-    echo -e "\e[1;33mWARN: $*\e[0;0m"
-}
+source ${SCRIPTS_DIR}/common.sh
 
 usage() {
     cat <<EOM
