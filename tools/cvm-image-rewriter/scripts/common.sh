56e3bb0c4a17894222091b9e85ed9622b482f1f7
Lu Ken <ken.lu@intel.com>
2023-12-23 12:25:01 +0800

    run.sh: add step to check whether required tools installed.
    Signed-off-by: Lu Ken <ken.lu@intel.com>

--- tools/cvm-image-rewriter/scripts/common.sh
+++ tools/cvm-image-rewriter/scripts/common.sh
@@ -19,3 +19,18 @@ error() {
 warn() {
     echo -e "\e[1;33mWARN: $*\e[0;0m"
 }
+
+# Check whether the tools are installed from the provided list
+#
+# args:
+#   array of tool name  -    the name list to be checked
+check_tools() {
+    arr=("$@")
+    is_missing=false
+    for i in "${arr[@]}";
+    do
+        [[ "$(command -v "$i")" ]] || { info "MISSING: $i is not installed" 1>&2 ; is_missing=true ;}
+    done
+
+    [[ $is_missing != true ]] || { error "Please install missing tools"; }
+}
