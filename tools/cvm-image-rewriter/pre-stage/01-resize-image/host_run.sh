29281c228397946cae9e5d57260ed3e574b9e640
Lu Ken <ken.lu@intel.com>
2023-12-23 13:28:16 +0800

    cvm-image-rewriter/pre-stag/01-resize-image: use environemnt variable GUEST_SIZE
    Check whether GUEST_SIZE is specified in environment, skip this plugin if not.
    
    Signed-off-by: Lu Ken <ken.lu@intel.com>

--- tools/cvm-image-rewriter/pre-stage/01-resize-image/host_run.sh
+++ tools/cvm-image-rewriter/pre-stage/01-resize-image/host_run.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-echo "Guest Image is at ${GUEST_IMG}..."
+CURR_DIR=$(dirname "$(readlink -f "$0")")
 
-qemu-img resize "${GUEST_IMG}" 50G
-virt-customize -a "${GUEST_IMG}" \
-        --run-command 'growpart /dev/sda 1' \
-        --run-command 'resize2fs /dev/sda1'
+# shellcheck disable=SC1091
+source "${CURR_DIR}/../../scripts/common.sh"
+
+info "Guest Image is at ${GUEST_IMG}..."
+
+if [[ -z ${GUEST_SIZE} ]]; then
+    warn "SKIP: Guest size is not defined via environment variable 'GUEST_SIZE'"
+else
+    qemu-img resize "${GUEST_IMG}" "${GUEST_SIZE}"
+    virt-customize -a "${GUEST_IMG}" \
+            --run-command 'growpart /dev/sda 1' \
+            --run-command 'resize2fs /dev/sda1'
+fi
