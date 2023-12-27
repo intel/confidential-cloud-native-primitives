89920a0841b93e5e612056f111ed4fa94bbbafd8
Ruoyu Ying <ruoyu.ying@intel.com>
2023-12-27 20:01:33 +0800

    example: refine shell script
    Signed-off-by: Ruoyu Ying <ruoyu.ying@intel.com>

--- deployment/script/deploy-and-exec-ccnp-example.sh
+++ deployment/script/deploy-and-exec-ccnp-example.sh
@@ -4,13 +4,44 @@
 
 set -e
 
+DEFAULT_DOCKER_REPO=docker.io/library
+DEFAULT_TAG=latest
+TEMP_MANIFEST_FILE=/tmp/ccnp-node-measurement-example-deployment.yaml
+
+usage() { echo "Usage: $0 [-r <registry-prefix>] [-g <image-tag>] [-i <register-index-to-verify>]"; exit 1; }
+while getopts ":r:g:i:h" option; do
+        case "${option}" in
+            r) registry=${OPTARG};;
+            g) tag=${OPTARG};;
+            i) index=${OPTARG};;
+            h) usage;;
+            *) echo "Invalid option: -${OPTARG}" >&2
+               usage
+               ;;
+        esac
+    done
+
 echo "Step 1:  Deploy CCNP example container for node measurement in Kubernetes"
-kubectl apply -f ../manifests/ccnp-node-measurement-example-deployment.yaml
+# replace registry and image tag according to user input
+cp ../manifests/ccnp-node-measurement-example-deployment.yaml $TEMP_MANIFEST_FILE
+if [[ -n "$registry" ]]; then
+	sed -i  "s#${DEFAULT_DOCKER_REPO}#${registry}#g" $TEMP_MANIFEST_FILE
+fi
+if [[ -n "$tag" ]];then
+	sed -i "s#${DEFAULT_TAG}#${tag}#g" $TEMP_MANIFEST_FILE
+fi
+kubectl apply -f $TEMP_MANIFEST_FILE
+sleep 3
+
 echo "Step 2:  Execute node measurement fetching and verification"
 POD_NAME=$(kubectl get po | grep ccnp-node-measurement-example | awk '{ print $1 }')
-if [ $# -eq 0 ]
+if [[ -z "$POD_NAME" ]]; then
+	echo "ccnp-node-measurement-example pod not found!"
+fi
+IFS=' ' read -r -a arr <<< "${index}"
+if [[ ${#arr[@]} -eq 0 ]]
 then
 	kubectl exec -it "$POD_NAME" -- python3 fetch_node_measurement.py
 else
-	kubectl exec -it "$POD_NAME" -- python3 fetch_node_measurement.py --verify-register-index "$@"
+	kubectl exec -it "$POD_NAME" -- python3 fetch_node_measurement.py --verify-register-index "${arr[@]}"
 fi
