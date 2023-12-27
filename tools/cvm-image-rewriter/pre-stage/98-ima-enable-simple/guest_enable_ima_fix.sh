55bebcc31a209dfdbf0814730b0108419452b7b6
Xiaocheng Dong <xiaocheng.dong@intel.com>
2023-12-27 16:21:48 +0800

    cvm-image-rewriter/ima: Change IMA updating flow to fix bugs
    1. Enable i_version and `ima_appraise=fix` before cloud-init process.
    2. Update IMA appraisal attributes in cloud-init.
    3. Change kernel parameter to `ima_appraise=log` after cloud-init.
    
    Signed-off-by: Xiaocheng Dong <xiaocheng.dong@intel.com>

--- tools/cvm-image-rewriter/pre-stage/98-ima-enable-simple/guest_enable_ima_fix.sh
+++ tools/cvm-image-rewriter/pre-stage/98-ima-enable-simple/guest_enable_ima_fix.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
 
+FSTAB_FILE=/etc/fstab
+FSTAB_FILE_NEW=$FSTAB_FILE".new"
+awk '{if($3 == "ext4" && $4 !~ /iversion/) $4 = $4",iversion"; print}' \
+    $FSTAB_FILE \
+    > $FSTAB_FILE_NEW
+mv $FSTAB_FILE_NEW $FSTAB_FILE
+
 GRUB_FILE=/etc/default/grub.d/50-cloudimg-settings.cfg
 GRUB_FILE_NEW=$GRUB_FILE".new"
 
-awk '{if($1 ~ /GRUB_CMDLINE_LINUX_DEFAULT/) sub("=\"","=\"ima_appraise=fix "); print}' \
+# Remove ima_appraise and rootflags if exists
+sed -i 's/ima_appraise=\(fix\|enforce\|log\|off\)//' $GRUB_FILE
+sed -i 's/rootflags=i_version//' $GRUB_FILE
+
+awk '{if($1 ~ /GRUB_CMDLINE_LINUX_DEFAULT/) sub("=\"","=\"ima_appraise=fix rootflags=i_version "); print}' \
     $GRUB_FILE \
     > $GRUB_FILE_NEW
 mv $GRUB_FILE_NEW $GRUB_FILE
