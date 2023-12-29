74a4fe67630c412cc3b02fc1775f22a7d46e8c26
Ruoyu Ying <ruoyu.ying@intel.com>
2023-12-29 10:50:45 +0800

    example: fix image name and example script
    * add default prefix for example image
    * increase script robustness
    
    Signed-off-by: Ruoyu Ying <ruoyu.ying@intel.com>

--- deployment/script/deploy-and-exec-ccnp-example.sh
+++ deployment/script/deploy-and-exec-ccnp-example.sh
@@ -7,13 +7,15 @@ set -e
 DEFAULT_DOCKER_REPO=docker.io/library
 DEFAULT_TAG=latest
 TEMP_MANIFEST_FILE=/tmp/ccnp-node-measurement-example-deployment.yaml
+DELETE_DEPLOYMENT=false
 
-usage() { echo "Usage: $0 [-r <registry-prefix>] [-g <image-tag>] [-i <register-index-to-verify>]"; exit 1; }
-while getopts ":r:g:i:h" option; do
+usage() { echo "Usage: $0 [-r <registry-prefix>] [-g <image-tag>] [-d delete existing deployment] [-i <register-index-to-verify>]"; exit 1; }
+while getopts ":r:g:i:dh" option; do
         case "${option}" in
             r) registry=${OPTARG};;
             g) tag=${OPTARG};;
             i) index=${OPTARG};;
+            d) DELETE_DEPLOYMENT=true;;
             h) usage;;
             *) echo "Invalid option: -${OPTARG}" >&2
                usage
@@ -30,14 +32,34 @@ fi
 if [[ -n "$tag" ]];then
 	sed -i "s#${DEFAULT_TAG}#${tag}#g" $TEMP_MANIFEST_FILE
 fi
+
+if [ $DELETE_DEPLOYMENT == true ]
+then
+    echo "==> Cleaning up ccnp-node-measurement-example deployment"
+    kubectl delete -f $TEMP_MANIFEST_FILE
+fi
+
+echo "==> Creating ccnp-node-measurement-example deployment"
 kubectl apply -f $TEMP_MANIFEST_FILE
-sleep 3
+for i in {1..5}
+do
+    POD_NAME=$(kubectl get po | grep ccnp-node-measurement-example | grep Running | awk '{ print $1 }')
+    if [[ -z "$POD_NAME" ]]
+    then
+        sleep 3
+        echo "Retrying $i time ..."
+    else
+        break
+    fi
+done
+
+if [[ -z "$POD_NAME" ]]; then
+    echo "No ccnp-node-measurement-example pod with status running! Please check your deployment."
+    exit 1
+fi
+echo ""
 
 echo "Step 2:  Execute node measurement fetching and verification"
-POD_NAME=$(kubectl get po | grep ccnp-node-measurement-example | awk '{ print $1 }')
-if [[ -z "$POD_NAME" ]]; then
-	echo "ccnp-node-measurement-example pod not found!"
-fi
 IFS=' ' read -r -a arr <<< "${index}"
 if [[ ${#arr[@]} -eq 0 ]]
 then
