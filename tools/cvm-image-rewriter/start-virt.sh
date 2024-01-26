73cdc2b9610afa50a2f62fa4aae85c00cdafea38
Hao, Ruomeng <ruomeng.hao@intel.com>
2024-01-26 15:21:06 +0800

    Update start-virt.sh 1. User can specify vCPU number and memory size 2. Add usage example in README.md
    Signed-off-by: Hao, Ruomeng <ruomeng.hao@intel.com>

--- tools/cvm-image-rewriter/start-virt.sh
+++ tools/cvm-image-rewriter/start-virt.sh
@@ -11,6 +11,8 @@ GUEST_NAME="tdx-guest"
 GUEST_ROOTDIR=/tmp/libvirt-vms
 TEMPLATE="${CURR_DIR}/tdx-libvirt-ubuntu-host.xml.template"
 FORCE=false
+VCPU_NUM=1
+MEM_SIZE=4
 
 usage() {
     cat << EOM
@@ -19,6 +21,8 @@ Usage: $(basename "$0") [OPTION]...
   -n <guest name>           Name of TD guest
   -t <template file>        Default is ./tdx-libvirt-ubuntu-host.xml.template
   -f                        Force recreate
+  -v <vcpu number>          VM vCPU number
+  -m <memory size in GB>    VM memory size in GB
   -h                        Show this help
 EOM
 }
@@ -32,11 +36,13 @@ pre-check() {
 }
 
 process_args() {
-    while getopts ":i:n:t:fh" option; do
+    while getopts ":i:n:t:v:m:fh" option; do
         case "$option" in
             i) GUEST_IMG=$OPTARG;;
             n) GUEST_NAME=$OPTARG;;
             t) TEMPLATE=$OPTARG;;
+            v) VCPU_NUM=$OPTARG;;
+            m) MEM_SIZE=$OPTARG;;
             f) FORCE=true;;
             h) usage
                exit 0
@@ -51,6 +57,7 @@ process_args() {
 
     echo "====================================================================="
     echo " Use template   : ${TEMPLATE}"
+    echo " Guest name     : ${GUEST_NAME}"
     echo " Guest XML      : ${GUEST_ROOTDIR}/${GUEST_NAME}.xml"
     echo " Guest Image    : ${GUEST_ROOTDIR}/${GUEST_NAME}.qcow2"
     echo " Force Recreate : ${FORCE}"
@@ -81,6 +88,20 @@ process_args() {
         echo "Please specify via -t"
         exit 1
     fi
+
+    # Validate the number of vCPUs
+    if ! [[ ${VCPU_NUM} =~ ^[0-9]+$ && ${VCPU_NUM} -gt 0 ]]; then
+        echo "Error: Invalid number of vCPUs: ${VCPU_NUM}"
+        usage
+        exit 1
+    fi
+
+    # Validate the size of memory
+    if ! [[ ${MEM_SIZE} =~ ^[0-9]+$ && ${MEM_SIZE} -gt 0 ]]; then
+        echo "Error: Invalid memory size: ${MEM_SIZE}"
+        usage
+        exit 1
+    fi
 }
 
 create-vm() {
@@ -88,11 +109,13 @@ create-vm() {
     echo "> Create ${GUEST_ROOTDIR}/${GUEST_NAME}.qcow2..."
     cp "${GUEST_IMG}" "${GUEST_ROOTDIR}/${GUEST_NAME}.qcow2"
     echo "> Create ${GUEST_ROOTDIR}/${GUEST_NAME}.xml..."
-    cp "${CURR_DIR}/${TEMPLATE}" "${GUEST_ROOTDIR}/${GUEST_NAME}.xml"
+    cp "${TEMPLATE}" "${GUEST_ROOTDIR}/${GUEST_NAME}.xml"
 
     echo "> Modify configurations..."
     sed -i "s/.*<name>.*/<name>${GUEST_NAME}<\/name>/" "${GUEST_ROOTDIR}/${GUEST_NAME}.xml"
     sed -i "s#/path/to/image#${GUEST_ROOTDIR}/${GUEST_NAME}.qcow2#" "${GUEST_ROOTDIR}/${GUEST_NAME}.xml"
+    sed -i "s/REPLACE_VCPU_NUM/${VCPU_NUM}/g" "${GUEST_ROOTDIR}/${GUEST_NAME}.xml"
+    sed -i "s/REPLACE_MEM_SIZE/${MEM_SIZE}/g" "${GUEST_ROOTDIR}/${GUEST_NAME}.xml"
 }
 
 start-vm() {
